f7d792ac930ef8e282b19920c33b78d9ce82e901
Anthony Villegas <villegasanthony87@yahoo.com>
2021-09-23 15:08:13 -0500

    feat: Input class set up

--- src/Input.java
+++ src/Input.java
@@ -4,11 +4,30 @@ public class Input {
 
     public Input() {
         this.scan = new Scanner(System.in);
-        scan.useDelimiter();
-    }
-
-    public String getString(String prompt){
 
     }
+
+    public String getString(String userInput){
+        System.out.println(userInput);
+        return this.scan.nextLine();
+    }
+
+    public int getNumber(String userInput, int min, int max) throws NumberFormatException {
+        System.out.println(userInput);
+        int num;
+        String select = this.scan.nextLine();
+        try {
+            num = Integer.parseInt(getString(userInput));
+            if(num > max || num < min) {
+                return getNumber("Input invalid, please try again", min, max);
+            }
+            return num;
+        } catch (NumberFormatException nox) {
+            nox.printStackTrace();
+        }
+        return getNumber("Uh Oh! Please try again", min , max);
+    }
+
+
 }
 
