38e4cbd779785c1c9a4795d4b221b2721846a198
Anthony Villegas <villegasanthony87@yahoo.com>
2021-09-24 13:32:28 -0500

    feat: implemented addContacts()

--- src/Input.java
+++ src/Input.java
@@ -14,9 +14,15 @@ public class Input {
 
     }
 
-    public String getString(String userInput){
-        System.out.println(userInput);
-        return this.scan.nextLine();
+    public String getString(){
+       String userInput = scan.next();
+        return userInput;
+    }
+
+    public boolean yesNo(){
+        System.out.println("Enter yes or y: ");
+        String userInput = scan.next().toLowerCase();
+        return (userInput.equals("yes") || userInput.equals("y"));
     }
 
     public int getNumber(String userInput, int min, int max) throws NumberFormatException {
@@ -41,39 +47,69 @@ public class Input {
         List<String> list = new ArrayList<>();
         try{
             list = Files.readAllLines(pathToList);
+
         } catch(IOException iox) {
             iox.printStackTrace();
         }
-
         System.out.println("Name | Phone Number");
         System.out.println("-------------------");
         for(String contact : list) {
             System.out.println(contact);
         }
+
+
+    }
+
+    public void addContact(){
+        Path pathToList = Paths.get("src", "contacts.txt");
+        Scanner name = new Scanner(System.in);
+        Scanner number = new Scanner(System.in);
+
+        List<String> newContact = new ArrayList<>();
+        try{
+            System.out.println("Enter new contact Name: ");
+            String newName = name.next();
+            System.out.println("Enter number for contact: EX(000)000-000 ");
+            String newNumber = number.next();
+            String createNum = newName + " | " + newNumber;
+            newContact.add(createNum);
+            Files.write(pathToList, newContact, StandardOpenOption.APPEND);
+        } catch (IOException iox) {
+            iox.printStackTrace();
+        }
+
+
     }
 
     public void mainPrompt() {
         Path pathToList = Paths.get("src", "contacts.txt");
-        Input main = new Input();
+        //Input main = new Input();
+        boolean user;
 
-        System.out.println("1 - View Contacts.");
-        System.out.println("2 - Add a new Contact.");
-        System.out.println("3 - Search by contact name.");
-        System.out.println("4 - Delete an existing contact.");
-        System.out.println("5 - Exit.");
-        System.out.println("Enter an option (1, 2, 3, 4 or 5)");
-        String userInput = scan.next();
-        //System.out.println(main.getString(userInput));
+        do{
+            System.out.println("1 - View Contacts.");
+            System.out.println("2 - Add a new Contact.");
+            System.out.println("3 - Search by contact name.");
+            System.out.println("4 - Delete an existing contact.");
+            System.out.println("5 - Exit.");
+            System.out.println("Enter an option (1, 2, 3, 4 or 5)");
+            String userInput = scan.next();
+            //System.out.println(main.getString(userInput));
 
 
-        switch (userInput) {
-            case "1" -> displayContacts();
-            case "2" -> System.out.println("The day will only get better, stay positive!");
-            case "3" -> System.out.println("Stay the course!");
-            case "4" -> System.out.println("Perhaps the day will get better.");
-            case "5" -> System.out.println("What can we do to make it better?");
-            default -> System.out.println("We don't have that");
-        }
+            switch (userInput) {
+                case "1" -> displayContacts();
+                case "2" -> addContact();
+                case "3" -> System.out.println("Stay the course!");
+                case "4" -> System.out.println("Perhaps the day will get better.");
+                case "5" -> System.out.println("What can we do to make it better?");
+                default -> System.out.println("We don't have that");
+            }
+            System.out.println("Continue?");
+            user = yesNo();
+
+        }while(user);
+
 
     }
 
